0ed385fc62d08c039b69ffecc3df664ff81c2dfd
Daris <darisomeragic7@gmail.com>
2022-09-30 22:09:09 +0200

    4.cas-javascript

---
+++ 4.cas.javascript/index.js
@@ -0,0 +1,176 @@
+
+// primer
+/*
+dayNum =Number(prompt("Unesite neki broj:"));
+day = "";
+
+
+
+switch (dayNum) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+    case 4:
+    case 5:
+        day = "Friday";
+    case 6:
+        day = "Saturday";
+        console.log("SUBOTA!!!");
+        console.log("SUBOTA!!!");
+        console.log("SUBOTA!!!");
+        console.log("SUBOTA!!!");
+        console.log("SUBOTA!!!");
+        console.log("SUBOTA!!!");
+        break;
+    default:
+        day = "pogresan broj";
+        break;
+}
+console.log(day);
+*/
+
+
+// 1. zadatak
+
+/*
+brSati =Number(prompt("Unesite neki broj do 24:"));
+switch (true) {
+    case brSati < 0:
+        console.log("Pogresan unos");
+        break;
+    case brSati <= 10:
+        console.log("JUTRO!!");
+        break;
+    case brSati <= 12:
+        console.log("PODNE!!");
+        break;
+    case brSati <= 18:
+        console.log("PREDVECE!!");
+        break;
+    case brSati <= 20:
+        console.log("VECE!!");
+        break;
+    case brSati <= 24:
+        console.log("NOC!!");
+        break;
+    default:
+        console.log("Pogresan unos===");
+        break;
+}
+*/
+
+
+// 3.zadatak
+
+/*
+br1 =Number(prompt("Unesite prvi broj:"));
+br2 = Number(prompt("Unesite prvi broj:"));
+br3 = Number(prompt("Unesite prvi broj:"));
+
+if (br1 >= br2 && br2 >= br3) {
+    br1 >= br2 >= br3
+    console.log(br1, br2, br3);
+}else if (br1 >= br3 && br3 >= br2) {
+    br1 >= br3 >= br2
+    console.log(br1, br3, br2);
+} else if (br2 >= br1 && br1 >= br3) {
+    br2 >= br1 >= br3
+    console.log(br2, br1, br3);
+} else if (br2 >= br3 && br3 >= br1) {
+    br1 >= br3 >= br2
+    console.log(br2, br3, br1);
+} else if (br3 >= br1 && br1 >= br2) {
+    br3 >= br1 >= br2
+    console.log(br3, br1, br2);
+} else if (br3 >= br2 && br2 >= br1) {
+    br3 >= br2 >= br1
+    console.log(br3, br2, br1);
+} else {
+    console.log("Pogresan unos");
+}
+*/
+
+
+// 4.zadatak
+
+/*
+mesec =Number(prompt("Unesite neki broj da nije veci od 12:"));
+if (mesec > 0 && mesec < 13) {
+switch (mesec) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+    console.log(`${mesec} mesec u godini ima 31 dan `);
+    break;
+    case 2:
+    console.log("Ima 28/9 dan");
+    break;
+    default:
+    console.log("Ima 30 dan");
+    break;
+}
+} else {
+console.log("pogresan unos");
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
