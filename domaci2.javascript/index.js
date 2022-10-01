a7d2ece8f20e6bd1c850fc0ca1c390d35be15223
Daris <darisomeragic7@gmail.com>
2022-10-01 16:55:35 +0200

    domaci2.javascript

---
+++ domaci2.javascript/index.js
@@ -0,0 +1,61 @@
+/*
+mesec = Number (prompt("Unesite broj meseca:"));
+if (mesec > 0 && mesec < 13){
+    switch(mesec){
+        case 1:
+        if (mesec === 1)
+        console.log("Januar");
+        case 2:
+        if (mesec === 2)
+        console.log("Februar");
+        case 3:
+        if (mesec === 3)
+        console.log("Mart");
+            console.log(`${mesec} mesec u godini pripada prvoj cetvrtini`);
+            break;
+        case 4:
+        if (mesec === 4)
+        console.log("April");
+        case 5:
+        if (mesec === 5)
+        console.log("Maj");
+        case 6:
+        if (mesec === 6)
+        console.log("Jun");
+            console.log(`${mesec} mesec u godini pripada drugoj cetvrtini`);
+            break;
+        case 7:
+        if (mesec === 7)
+        console.log("Jul");
+        case 8:
+        if (mesec === 8)
+        console.log("Avgust");
+        case 9:
+        if (mesec === 9)
+        console.log("Septembar");
+            console.log(`${mesec} mesec u godini pripada trecoj cetvrtini`);
+            break;
+        case 10:
+        if (mesec === 10)
+        console.log("Oktobar");
+        case 11:
+            if (mesec === 11)
+        console.log("Novembar");
+        case 12:
+            if (mesec === 12)
+        console.log("Decembar");
+            console.log(`${mesec} mesec u godini pripada cetvrtoj cetvrtini`);
+            break;
+}
+}else {
+    console.log("Pogresan unos")
+}
+*/
+
+
+
+
+
+
+
+
