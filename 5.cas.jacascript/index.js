c83eb20da54940075c8b3fd1648bdc439f119f38
Daris <darisomeragic7@gmail.com>
2022-10-03 21:21:05 +0200

    5.cas.javascript

---
+++ 5.cas.jacascript/index.js
@@ -0,0 +1,48 @@
+
+/*
+
+for (i=6; i>0; i--){
+    console.log(i);
+}
+*/
+
+/*
+for (i=0; i<10; i++){
+    if (i!==7){
+        console.log(i)
+    }
+}
+*/
+
+
+/*
+numSum=0;
+for (i=0; i<21;i++){
+    if(i % 2 === 0){
+        numSum=numSum+i;
+        console.log(`Nakon broja ${i},suma je ${numSum}`)
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
