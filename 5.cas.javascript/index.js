b997f333af309a40998d492dbc7718a6080929d1
Daris <darisomeragic7@gmail.com>
2022-10-05 23:06:49 +0200

    for-while-petlja

---
+++ 5.cas.javascript/index.js
@@ -0,0 +1,76 @@
+
+
+/*
+n=Number(prompt("Unesite neki prirodni broj:"));
+for (i=1; i<n; i++ ){
+    if (n % i===0){
+        console.log(`Broj koji je deljiv sa ${n} je ${i}`)
+    }
+}
+*/
+
+/*
+for (i=0; i<11; i++){
+    i=Number(prompt("Unosimo policu broj"));
+    console.log("Unosimo policu broj: " + i); 
+}
+*/
+
+/*
+broj=Number(prompt("Unesite neki broj:"));
+
+for (i=0; i<= broj; i+=2){
+    console.log(`Parni brojevi broja ${broj} su ${i}`)
+    }
+*/
+
+/*
+broj=Number(prompt("Unesite neki broj:"))
+
+for (i=0; i<=broj;i++){
+    console.log(i)
+}
+*/
+
+/*
+broj=Number(prompt("Unesite neki broj:"));
+
+for (i=0;i<=broj;i++){
+    if (i % 2 ===0)
+    console.log(i)
+}
+*/
+
+/*
+brojIteracija=5;
+
+while (brojIteracija > 0){
+    console.log("PETLJA");
+    brojIteracija--
+}
+*/
+
+
+
+/*
+lozinka="tajno"
+
+korLozinka="";
+
+while(korLozinka !==lozinka){
+    korLozinka=prompt("Unesite sifru:");
+}
+console.log("Tacna sifra")
+*/
+
+
+
+
+
+
+
+
+
+
+
+
