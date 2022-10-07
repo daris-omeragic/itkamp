801b770af4eb821b0070b839ae8e8b890778dc74
Daris <darisomeragic7@gmail.com>
2022-10-07 22:41:33 +0200

    javascript

---
+++ 6.cas.javascript/index.js
@@ -0,0 +1,73 @@
+/*
+mojaArr=["value1", "value2", false, 1321];
+//         0          1       2      3      
+
+
+
+console.log(mojaArr[2]);
+grupa11=["Ime1","Ime2","Ime2","ime3","ime4","ime5","ime6"]
+
+//grupa11.push("nova vrednost");
+grupa11.pop()
+mojaVar=grupa11.pop()
+
+
+console.log(grupa11)
+console.log(mojaAar)
+
+for(let i = 0; i<grupa11.length;i++){
+}
+
+*/
+/*
+for(let i = 0; i < 6; i++){
+    console.log(grupa11[i],i)
+}
+*/
+
+
+/*
+niz1=[]
+
+for (i=0; i < 10; i++){
+    unos=Number(prompt("Unesite elemente nizova:"))
+    niz1.push(unos)
+}
+
+for (let i = 0; i < niz1.length; i++) {
+    if(niz1[i]%2===0){
+        console.log(niz1[i])
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
