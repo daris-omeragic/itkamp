291758c02ab2107161e4f176c1ea761b7091a5cb
Daris <darisomeragic7@gmail.com>
2022-10-10 21:48:58 +0200

    7.cas-javascript

---
+++ 7.cas.javascript/index.js
@@ -0,0 +1,82 @@
+/*
+let elements=[70,2,3,4,5,6];
+// dodaje na pocetak niza element
+elements.unshift(11);
+
+console.log(elements.unshift(11));
+console.log(elements);
+
+
+*/
+
+
+/*
+let elements=[70,2,3,4,5,6];
+
+// element koji smo ukonili iz niza i cuva (uklanja prvi element)
+let pom=elements.shift();
+console.log(`Element koji je izbacen je ${pom}`)
+console.log(elements)
+*/
+
+
+/*
+function doSmth(){
+    console.log("Hello")
+}
+*/
+
+
+/*
+for (let i = 0; i < 5; i++){
+    doSmth();
+    doSmth();
+}
+*/
+
+/*
+const animals=["ant","bison","camel","duck","elephant"];
+
+//                    start,and
+console.log(animals.slice(0,3));
+console.log(animals)
+
+*/
+
+
+/*
+const months=["Januar","March","April","June"]
+console.log(months)
+
+months.splice(1,0,"Februar")
+
+console.log(months)
+*/
+
+
+
+/*
+let broj;
+let niz=[];
+
+while(true){
+    broj=Number(prompt("Unesite broj:"))
+    if (broj % 15===0){
+    break;
+}   niz.push(broj)
+}
+console.log(niz)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
