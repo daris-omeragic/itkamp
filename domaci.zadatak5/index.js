03556145ecc41b495ad7caf066723d921f5f7af0
Daris <darisomeragic7@gmail.com>
2022-10-11 21:50:23 +0200

    domaci.javascript

---
+++ domaci.zadatak5/index.js
@@ -0,0 +1,32 @@
+
+/*
+let broj;
+
+let niz = [];
+
+
+while (true) {
+    broj = Number(prompt("Unesite broj:"));
+    if (broj % 3 === 0 && broj % 5 === 0) {
+        console.log(`FIZZBUZZ (${broj}) `)
+        break;
+    }
+    if (broj % 3 === 0) {
+        console.log(`FIZZ (${broj})`);
+    } if (broj % 5 === 0) {
+        console.log(`BUZZ (${broj})`);
+    }
+    niz.push(broj)
+}
+console.log(niz)
+*/
+
+
+
+
+
+
+
+
+
+
