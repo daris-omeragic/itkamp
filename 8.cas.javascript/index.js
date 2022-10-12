ffb0fbff5da029074a76ac2e1795f94abafbe584
Daris <darisomeragic7@gmail.com>
2022-10-12 22:43:42 +0200

    javascript

---
+++ 8.cas.javascript/index.js
@@ -0,0 +1,107 @@
+/*
+arr=[[34,45,67],[true,123],false,123];
+
+
+console.log(arr[arr.length-1]);
+
+console.log(arr[1][0]);
+*/
+
+/*
+arr=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+];
+
+
+
+for (let i = 0; i<3;i++) {
+    for(let j = 0; j<3;i++){
+        console.log(arr[i][j]);
+    }
+}
+*/
+
+
+
+/*
+niz=[
+    [18,23,32],
+    [41,56,67],
+    [74,84,93]
+];
+
+
+
+for (let i = 0; i<niz.length ; i++){
+    let red="";
+    for(let j = 0; j<niz.length; j++ ){
+        red=red+niz[i][j]+ " "
+    }
+    console.log(red)
+}
+*/
+
+
+
+
+
+
+
+
+/*
+color=["Blue","Green","Red","Orange","Violet","Indigo","Yellow"];
+o=["th","st","nd","rd"];
+
+
+for (let i = 0 ; i < color.length; i++){
+    if (i === 0){
+        console.log(i+1, o[1],"choice is",color[i]);
+    }else if(i === 1){
+        console.log(i+1, o[2],"choice is",color[i]);
+    }else if(i === 2){
+        console.log(i+1, o[3],"choice is",color[i])
+    }else{
+        console.log(i+1, o[0],"choice is",color[i])
+    }
+}
+*/
+
+
+
+/*
+let arr3 = [1,2,3,4,5];
+let arr4 = [5,4,3,2,1,10];
+let results = [];
+
+
+for (let i = 0; i < arr4.length; i++){
+    if (!(arr3[i] === undefined)) results.push (arr3[i] + arr4[4]); 
+    else{
+        results.push(arr4[i]);
+    }
+}
+console.log(results);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
