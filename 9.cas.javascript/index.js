45ae94e2c2f0abbceda08cfdb0baa5f64b8e7ee6
Daris <darisomeragic7@gmail.com>
2022-10-14 22:26:32 +0200

    javascript

---
+++ 9.cas.javascript/index.js
@@ -0,0 +1,100 @@
+
+
+/*niz = [
+    [34, 56, 23],
+    [78, 45, 23],
+    [65, 32, 87],
+]
+
+
+
+for (let i = 0; i < niz.length; i++) {
+    red = ""
+    for (let j = 0; j < niz[i].length; j++) {
+        if (i === j)
+        red = red + niz[i][j] + " "
+    }else {
+        red+="   "
+    }
+    console.log(red);
+}
+
+*/
+
+
+
+
+
+
+/*
+niz = [
+    [34, 56, 23],
+    [78, 45, 23],
+    [65, 32, 87],
+]
+
+
+
+for (let i = 0; i < niz.length; i++) {
+    red = ""
+    for (let j = 0; j < niz[i].length; j++) {
+        if (i+j === niz.length-1)
+        red = red + niz[i][j] + " ";
+        else{
+            red+="   "
+        }
+    }
+    console.log(red);
+}
+*/
+
+
+/*
+
+niz=[];
+
+niz1=[]
+
+niz2=[]
+
+for (let i= 1; i < 101; i++) {
+    if(!(i%2===0)) {
+        niz.push(i)
+    }if (i % 2 ===0 && i % 5===0){
+        niz1.push(i)
+    }if (i % 3 === 0){
+        i**2
+        niz2.push(i)
+    }
+}
+
+console.log(niz)
+console.log(niz1)
+console.log(niz2)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ vezbanje.javascript.zadaci/index.js
@@ -0,0 +1,71 @@
+// Write a simple Javascript program to join all elements of the following array into a string
+
+/*
+
+color=["Red","Green","White","Black"];
+
+
+console.log(color.toString());
+console.log(color.join());
+console.log(color.join("+"));
+
+*/
+
+
+
+//Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers.
+//For example if you accept 025468 the output should be 0-254-6-8. 
+
+/*
+var num = window.prompt();
+var str = num.toString();
+var result = [str[0]];
+
+for (var x = 1; x < str.length; x++) {
+
+    if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
+        result.push('-', str[x]);
+    } else {
+        result.push(str[x]);
+    }
+}
+console.log(result.join(''));
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
