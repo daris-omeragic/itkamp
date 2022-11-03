d7c9b54aed5adb91033482f7b325ffa0ae701a36
Daris <darisomeragic7@gmail.com>
2022-11-04 00:19:04 +0100

    javascript

---
+++ vezbanje.javascript.3/index.js
@@ -0,0 +1,105 @@
+
+
+
+
+
+/*
+const Osoba = {
+    firstName:"Daris",
+    lastName: "Omeragic",
+    phone : "062 18 28 503",
+    getElementAndPhone: function(){
+        console.log(`${this.firstName} - ${this.phone}`)
+    }
+
+}
+
+Osoba.getElementAndPhone()
+*/
+
+/*
+function Osoba(firstName, lastName, phone) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.getNameAndPhone = function () {
+        console.log(` ${firstName} - ${phone}`)
+    }
+}
+
+let osoba1 = new Osoba("Daris", "Omeragic", "062 18 28 503");
+let osoba2 = new Osoba("Nikola", "Nikolic", "063 46 75 239");
+
+
+osoba1.getNameAndPhone();
+osoba2.getNameAndPhone();
+*/
+
+
+
+
+/*
+class Osoba {
+    constructor(firstName, lastName, phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+    getNameAndPhone() {
+        console.log(`${this.firstName} - ${this.phone}`)
+    }
+}
+
+
+let osoba1 = new Osoba("Daris", "Omeragic", " 062 18 28 503");
+let osoba2 = new Osoba("Nikola", "Nikolic", " 063 21 48 121");
+
+
+
+console.log(osoba1);
+osoba1.getNameAndPhone();
+console.log(osoba2);
+osoba2.getNameAndPhone();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var library = [
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }];
+
+for (var i = 0; i < library.length; i++) {
+    var book = "'" + library[i].title + "'" + ' by ' + library[i].author + ".";
+    if (library[i].readingStatus) {
+        console.log("Already read " + book);
+    } else {
+        console.log("You still need to read " + book);
+    }
+}
+*/
+
+
