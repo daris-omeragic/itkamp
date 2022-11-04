b788c9a8ea660a0e1a91b4a04a09257307342e96
Daris <darisomeragic7@gmail.com>
2022-11-05 00:00:48 +0100

    javascript

---
+++ vezbanje.javascript.4/index.js
@@ -0,0 +1,120 @@
+
+
+
+/*
+user = {
+    firstName:"Daris",
+    lastName:"Omeragic",
+    sifra :"123",
+    years:"16"
+}
+
+
+
+
+arrKeys = Object.keys(user);
+console.log("arrKeys",arrKeys);
+
+
+for (let i = 0; i < arrKeys.length; i++) {
+    console.log(arrKeys[i],":" ,user[arrKeys[i]])
+}
+
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+arr2 = []
+
+
+arr = [
+    {
+        name:"Book1",
+        price:43,
+        quantity:3,
+    },
+    {
+        name:"Book2",
+        price:71,
+        quantity:4,
+        
+    },
+    {
+        name:"Book3",
+        price:22,
+        quantity:2,
+    },
+    {
+        name:"Book4 ",
+        price:99,
+        quantity:1,
+    },
+]
+
+
+
+sum = 0;
+
+for(book of arr){
+    console.log(book.price)
+    sum+=book.price * book.quantity
+}
+
+
+console.log(`Suma je`, sum)
+
+
+for(book of arr){
+    if(book.price > 50){
+        arr2.push(book)
+    }
+}
+
+console.log(arr2)
+
+
+*/
+
+
+
+
+/*
+obj1 = {
+    sifra: "1234",
+    description: "black and white",
+    lastName: "Omeragic",
+    firstName: "Daris",
+};
+
+
+obj2 = {
+    firstName: "Daris",
+    sifra: "1234",
+    description: "black and white",
+    lastName: "Omeragic",
+}
+
+keys = Object.keys(obj1);
+isEqual = true;
+
+for (key of keys) {
+    if (obj1[key] !== obj2[key]) {
+        isEqual = false;
+        break
+    }
+}
+
+console.log("isEqual", isEqual)
+a = obj2 = obj1
+console.log(obj1 === obj2)
+*/
+
+
