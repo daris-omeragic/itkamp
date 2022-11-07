f9e48211fa09d07f3077e5e3db7ee6dc1e19885e
Daris <darisomeragic7@gmail.com>
2022-11-07 21:35:13 +0100

    javascript

---
+++ domaci.zadatak8/index.js
@@ -0,0 +1,146 @@
+
+/*
+car = {
+    price: "36 000 $",
+    brand: "bmw",
+    color: "black",
+}
+
+
+function checkingProperties(car) {
+    property = "black";
+    found = "false";
+    for (key in car) {
+        if (car[key] === property) {
+            found = true;
+            break
+        }
+    }
+    return found;
+}
+
+console.log(checkingProperties(car));
+*/
+
+
+
+
+
+
+
+
+/*
+red = ""
+
+var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 };
+
+
+for (keys in student){
+    red=red+keys + ","
+}
+console.log(red)
+
+
+*/
+
+
+/*
+
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 
+};
+console.log(student)
+
+a = delete student.rollno
+
+console.log(student)
+
+*/
+
+
+
+
+/*
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12, 
+};
+
+
+
+var size = Object.keys(student).length;
+
+console.log(`The length of this object is ${size}`)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+String.prototype.sub_String = function () {
+    var subset = [];
+    for (var m = 0; m < this.length; m++) {
+        for (var n = m + 1; n < this.length + 1; n++) {
+            subset.push(this.slice(m, n));
+        }
+    }
+    return subset;
+};
+
+console.log("dog".sub_String());
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
