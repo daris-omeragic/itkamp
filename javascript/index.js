ad783908861df8271afcc0b7fc5839587c23e056
Daris <darisomeragic7@gmail.com>
2022-12-02 15:48:53 +0100

    javascript

---
+++ javascript/index.js
@@ -0,0 +1,58 @@
+/*
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+.then(function(res){
+    console.log(res)
+})
+.catch((err)=>{
+    console.log("Prikazati error tas")
+    console.log(err.message)
+}
+)
+*/
+
+
+
+
+
+/*
+
+nekiPosao = new Promise((resolve, reject) => {
+    const statusCode = 200;
+    if (statusCode === 200) {
+        resolve({
+            statusCode,
+            message: "Uspesno izvrsen posao",
+            data: [1, 23, 4, 54],
+        });
+    }
+    reject({
+        statusCode,
+        message: "Greska pri izvrsenju posla",
+    });
+})
+
+
+nekiPosao 
+.then((result) => {
+    console.log(result)
+}).catch((err) => {
+    console.log(err.message)
+});
+
+*/
+
+
+
+
+/*
+fetch("https://go-apod.herokuapp.com/apod")
+.then(function(res){
+    console.log(res)
+})
+.catch((err)=>{
+    console.log("Prikazati error tas")
+    console.log(err.message)
+}
+)
+
+*/
