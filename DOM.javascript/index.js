81b72164eaa519c4330870cbda4eeacd6e5e7353
Daris <darisomeragic7@gmail.com>
2022-12-05 22:51:46 +0100

    javascript

---
+++ DOM.javascript/index.js
@@ -0,0 +1,73 @@
+/*
+naslov = document.getElementById("naslov");
+console.log(naslov);
+
+
+
+
+
+a = document.getElementsByTagName("h1");
+
+console.log(a)
+
+
+
+b = document.querySelector("div h1");
+
+console.log(b)
+
+
+
+
+c = document.querySelectorAll("div h1");
+
+
+console.log(c);
+*/
+
+/*
+
+naslov = document.getElementById("naslov");
+
+
+
+naslov.innerText = "Nesto"
+
+
+naslov.style.color = "green"
+
+
+naslov.style.backgroundColor = "yellow"
+
+*/
+
+/*
+
+function changeColor(){
+    console.log("changeColor")
+    naslov = document.getElementById("naslov");
+    //naslov.style.color = "red";
+    if (naslov.style.color === "red"){
+        naslov.style.color = "blue"
+    } else{
+        naslov.style.color = "red"
+    }
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- javascript/index.js
+++ javascript/index.js
@@ -43,16 +43,174 @@ nekiPosao
 
 
 
-
 /*
-fetch("https://go-apod.herokuapp.com/apod")
+
+fetch("https://go-apod.herouapp.com/apod")
 .then(function(res){
     console.log(res)
 })
 .catch((err)=>{
-    console.log("Prikazati error tas")
     console.log(err.message)
-}
-)
+})
 
 */
+
+
+
+
+/*
+BASE_URL = "https://restcountries.com/v3.1/name/Aruba";
+arr = [];
+
+async function getData() {
+    const response = await fetch(BASE_URL);
+    const data = await response.json();
+    return data;
+}
+async function getMoreData() {
+    const countries = await getData(BASE_URL);
+
+    return countries;
+}
+const variables = arr.map(function (data) {
+    data.forEach((element) => {
+        console.log(element);
+    });
+});
+
+getMoreData().then((data) => {
+    data.forEach((c) => {
+        console.log(c.name.official);
+    });
+});
+*/
+
+
+
+
+
+
+// 1 B 
+/*
+async function getData() {
+    fetch ("https://api.thecatapi.com/v1/breeds")
+    .then(function(data){
+        console.log(data)
+    }
+    )
+}
+async function getMoreData() {
+    const cats = await getData("https://cdn2.thecatapi.com/images/0SxW2SQ_S.jpg")
+    return cats
+}
+
+
+getMoreData()
+*/
+
+/*
+
+async function getData() {
+    const response = await fetch("https://api.thecatapi.com/v1/breeds")
+    const data = await response.json()
+    return data
+}
+async function getMoreData() {
+    const cats = await getData("https://api.thecatapi.com/v1/breeds")
+    return cats
+}
+getMoreData().then(data => {
+    console.log(data)
+})
+
+
+
+getMoreData().then((data) => {
+    data.forEach((c) => {
+        console.log(c.name);
+    });
+});
+*/
+
+
+
+
+
+
+// C
+
+/*
+const arr = [];
+
+async function getData() {
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    const data = await response.json()
+    return data
+}
+
+
+async function getMoreData() {
+    const countries = await getData("https://restcountries.com/v3.1/all")
+    return countries
+}
+
+const variables = arr.map(function (data) {
+    let area = data.sort((a, b) => b - a).slice(0, 1)
+    console.log(area)
+    let population = data.sort((a, b) => b - a).slice(0, 1)
+    console.log(population)
+})
+
+getMoreData().then((data) => {
+    data.forEach((c) => {
+        console.log(c.population);
+    });
+});
+*/
+
+
+
+
+
+/*
+
+
+function job() {
+    return Promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello World!");
+        }, 2000);
+        reject("it's not working...")
+    });
+}
+
+
+
+async function job1() {
+    console.log('hello world');
+}
+
+job()    
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
