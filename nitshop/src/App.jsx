a32ed18a7a61dd9ec54e7ce6c85017e5c328e529
Daris <darisomeragic7@gmail.com>
2023-01-19 23:58:12 +0100

    update

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -1,9 +1,57 @@
 import { useState, useEffect } from "react";
+//import { useState,useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StyledButton from './components/styledButton/StyledButton';
 import StyleHeader from './components/styledButton/StyleHeader';
 import Text from './components/styledButton/StyleText';
+
+
+
+/*
+function ExampleFunctionalComponent() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  useEffect(() => {
+    if (age < 0 || age > 100) {
+      setAge(0);
+    }
+  }, [age]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleAgeChange(event) {
+    setAge(event.target.value);
+  }
+
+
+  return (
+    <form>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={handleNameChange} />
+      </label>
+      <br />
+      <label>
+        Age:
+        <input type="number" value={age} onChange={handleAgeChange} />
+      </label>
+    </form>
+  );
+}
+
+*/
+
+
+
+
+
+
+
+
+
 
 function App() {
   const [innerText, setInnerText] = useState("");
@@ -41,3 +89,5 @@ function App() {
 
 
 export default App;
+
+//export default ExampleFunctionalComponent;
