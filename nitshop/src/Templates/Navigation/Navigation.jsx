c9f27dbeea820ae6fe2e0ee71823f762b7290c3e
Daris <darisomeragic7@gmail.com>
2023-02-10 23:35:12 +0100

    navigation bar

--- nitshop/src/Templates/Navigation/Navigation.jsx
+++ nitshop/src/Templates/Navigation/Navigation.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Grid from "@mui/material/Grid"
-import CustomDiv from "../../components/CustomDiv/CustomDiv"
+//import CustomDiv from "../../components/CustomDiv/CustomDiv"
+import TopRow from "./TopRow"
 
 
 const Navigation = () => {
     return (
         <Grid container direction="row">
-            <CustomDiv padding ='0px 5%' border='0px'width='100%'>
-            </CustomDiv>
+            <TopRow/>
         </Grid>
     );
 }
+
 
 
 export default Navigation;
--- nitshop/src/Templates/Navigation/TopRow.jsx
+++ nitshop/src/Templates/Navigation/TopRow.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import  Grid  from '@mui/material/Grid';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import CustomDiv from "../../components/CustomDiv/CustomDiv";
+import { colors, fontSize } from "../../util/theme";
+import PhoneIcon from "@mui/icons-material/Phone";
 
-
-
-const TopRow = ()=>{
-    return (
-        <Grid container direction={"row"}></Grid>
-    )
+const TopRow = () => {
+  return (
+    <CustomDiv
+      bgColor={colors.secondColor}
+      width='100%'
+      height='30px'
+      padding='0px 5%'
+    >
+      <Grid container direction='row'>
+        <Grid item md={6} lg={6} spacing={0}>
+          <CustomDiv display='flex' alignItems='center'>
+            <PhoneIcon
+              style={{
+                fontSize: fontSize.normal,
+              }}
+            />
+            <p>+381 62 18 28 503</p>
+          </CustomDiv>
+        </Grid>
+        <Grid item md={6} lg={6} spacing={0}>
+          <CustomDiv width='50%' border='0px' bgColor='inherit'></CustomDiv>
+        </Grid>
+      </Grid>
+    </CustomDiv>
+  );
 };
-
 
 export default TopRow;
